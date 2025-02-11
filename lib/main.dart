01bd266ce4b9ffceaadc795e1127265c1b782153
vyada <vyadav1267354@gmail.com>
2025-02-11 16:37:59 +0530

    vin

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:shamstir/view/mainScreen.dart';
+import 'package:shamstir/view/splashScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool showSplash = true;
+  showSplashScreen(){
+    Future.delayed(Duration(seconds: 5),(){
+      setState(() {
+        showSplash = false;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    showSplashScreen();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'UnSad',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  showSplash ? splashScreen() : MainScreen(),
+    );
+  }
+}
